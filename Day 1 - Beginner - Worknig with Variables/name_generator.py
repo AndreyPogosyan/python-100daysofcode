3725653d1ef9ed4492f06d4dfb6d702d95aee474
Andrey Pogosyan <47284932+AndreyPogosyan@users.noreply.github.com>
2023-02-22 12:03:42 -0500

    Created python band name generator app

---
+++ Day 1 - Beginner - Worknig with Variables/name_generator.py
@@ -0,0 +1,9 @@
+# print function to display welcome message
+print("Welcome to the Band name generator")
+
+# Variables with input function to capture city name and pet name
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+
+# Print final message with generated band name
+print("Your band name could be " + city + " " + pet_name)
